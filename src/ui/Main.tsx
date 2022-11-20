f7dd531c93a1888190efbe4873c14b4f7163ec12
오현택 <bee.tools.rock@gmail.com>
2022-11-20 23:15:13 +0900

    modify: Main part basic flow test

--- src/ui/Main.tsx
+++ src/ui/Main.tsx
@@ -1,8 +1,57 @@
-import React from "react";
-import styles from "./Main.module.css";
+import React, { useCallback } from "react";
+
+import ReactFlow, {
+  MiniMap,
+  Controls,
+  Background,
+  useNodesState,
+  useEdgesState,
+  addEdge,
+} from "reactflow";
+
+import "reactflow/dist/style.css";
+
+const initialNodes = [
+  {
+    id: "1",
+    data: { label: "Hello" },
+    position: { x: 0, y: 0 },
+    type: "input",
+  },
+  {
+    id: "2",
+    data: { label: "World" },
+    position: { x: 100, y: 100 },
+  },
+];
+
+const initialEdges = [
+  { id: "1-2", source: "1", target: "2", label: "to the", type: "step" },
+];
 
 const Main = () => {
-  return <div>Main part</div>;
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+  const onConnect = useCallback(
+    (params: any) => setEdges((eds) => addEdge(params, eds)),
+    [setEdges]
+  );
+
+  return (
+    <ReactFlow
+      nodes={nodes}
+      edges={edges}
+      onNodesChange={onNodesChange}
+      onEdgesChange={onEdgesChange}
+      onConnect={onConnect}
+      fitView
+    >
+      <MiniMap />
+      <Controls />
+      <Background />
+    </ReactFlow>
+  );
 };
 
 export default Main;
